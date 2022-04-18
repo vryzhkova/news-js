36b6b24f5cfc460b0c8d94d6c7c7cd26f4b8d4e2
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-04-18 11:22:20 +0400

    added render news

--- js/script.js
+++ js/script.js
@@ -1,8 +1,52 @@
+const API_KEY = 'a0d254b2b61f4e92894608914b153659';
 const choicesElem = document.querySelector('.js-choice');
-
-console.log(choicesElem);
+const newsList = document.querySelector('.news-list');
 
 const choices = new Choices(choicesElem, {
     searchEnabled: false,
     itemSelectText: '',
 });
+
+const getdata = async (url) => {
+    const response = await fetch(url, {
+        headers: {
+            'X-Api-Key': API_KEY,
+        }
+    });
+
+    const data = await response.json();
+    
+    return data;
+};
+
+const renderCard = (data) => {
+    newsList.textContent = '';
+
+    data.forEach(news => {
+        const card = document.createElement('li');
+        card.className = 'news-item';
+
+        card.innerHTML = `
+                        <img src="${news.urlToImage}" alt="${news.title}" class="news-image" width="270" height="200">
+
+                        <h3 class="news-title">
+                            <a href="${news.url}" class="news-link" target="_blank">${news.title}</a>
+                        </h3>
+                        <p class="news-description">${news.description}</p>
+                        <div class="news-footer">
+                            <time class="news-datetime" datetime="${news.publishedAt}">
+                                <span class="news-date">${news.publishedAt}</span> 11:06
+                            </time>
+                            <div class="news-author">${news.author}</div>
+                        </div>
+        `;
+        newsList.append(card);
+    })
+};
+
+const logNews = async () => {
+    const data = await getdata('https://newsapi.org/v2/top-headlines?country=ru');
+    renderCard(data.articles);
+};
+
+logNews();
